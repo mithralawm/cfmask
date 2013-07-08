7918190ede957ce5476c71606d07883f654187a5
schmidtga00@gmail.com <schmidtga00@gmail.com@8aa44e39-407d-e975-90f4-c7438b917e6c>
2013-07-08 17:27:10 +0000

    Updated this app to read the cfmask version and append it to the global variables in the updated lndsr product.  Standardized some error variables.

--- append_cfmask/trunk/src/append_cfmask.c
+++ append_cfmask/trunk/src/append_cfmask.c
@@ -11,10 +11,19 @@
  *  updated lndsr* file in HDF format with cfmask band appended
  *
  * !Developer: 
- *  Gail Schmidt, USGS EROS LSRD project
+ *  Gail Schmidt, USGS EROS LSRD project  6/11/2013
+ *  Quick and dirty application to handle the append of the cfmask cloud
+ *    mask to the lndsr surface reflectance product.  This will be replaced
+ *    when the project migrates to raw binary as the internal file format.
+ *    Used lndapp application from LEDAPS as the baseline for this application.
  *
  * !History 
- *  Used lndapp application from LEDAPS as the baseline for this application.
+ *  Gail Schmidt, USGS EROS LSRD project   7/8/2013
+ *  Updated to read the CFmaskVersion from the cfmask file and append it to
+ *    the lndsr global attributes.
+ *  Modified FAILURE and SUCCESS to be standard values instead of using the
+ *    HDF and HDF-EOS failure value.
+ *
  */
 
 #include <stdio.h>
@@ -27,8 +36,9 @@
 
 #define MAX_NCOLS   10000 
 #define Max_StrLen  1000
-#define FAILURE     -1
-#define SUCCESS     1
+#define HDF_FAILURE -1
+#define FAILURE     1
+#define SUCCESS     0
 
 typedef struct {
 
@@ -40,6 +50,7 @@ typedef struct {
 
   int32 SD_ID;            
   int32 band_id; 
+  char    versionNum[Max_StrLen];
 
   char    sdsName[Max_StrLen];
   char    longName[Max_StrLen];
@@ -52,11 +63,11 @@ typedef struct {
 } GRID_SR;    
 
 void usage(char *);
-int  getMetaInfo(GRID_SR *);
+int  getMetaInfo(GRID_SR *sr, bool processSR);
 int  openForWrite(GRID_SR *sr, GRID_SR *cfmask);
 int  closeUp(GRID_SR *sr, bool close);
 
-#define DEBUG 1
+//#define DEBUG 1
 
 int main(int argc, char *argv[])
 {
@@ -68,7 +79,7 @@ int main(int argc, char *argv[])
 
   if(argc != 5) {
     usage(argv[0]);
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   
   /* parse command line */
@@ -81,7 +92,7 @@ int main(int argc, char *argv[])
       else{
 	printf("\nWrong option:%s\n",argv[i]);
 	usage(argv[0]);
-	exit(1);
+	exit(EXIT_FAILURE);
       }    
   }
 
@@ -89,41 +100,43 @@ int main(int argc, char *argv[])
   strcpy(cfmask.sdsName, "fmask_band");
 
   /* get metadata from lndsr* file */
-  if((ret=getMetaInfo(&sr))==FAILURE) {
+  printf ("Reading meta info from SR file ...\n");
+  if((ret=getMetaInfo(&sr, true))==FAILURE) {
     printf("Retrieve %s metadata error\n", sr.fileName);
-    exit(1);
+	exit(EXIT_FAILURE);
   }
+  printf ("\n");
 
   /* get metadata from cfmask* file */
-  if((ret=getMetaInfo(&cfmask))==FAILURE) {
+  printf ("Reading meta info from fmask file ...\n");
+  if((ret=getMetaInfo(&cfmask, false))==FAILURE) {
     printf("Retrieve %s metadata error\n", cfmask.fileName);
-    exit(1);
+	exit(EXIT_FAILURE);
   }
+  printf ("\n");
   
   if(sr.nrows!=cfmask.nrows || sr.ncols!=cfmask.ncols || sr.ulx!=cfmask.ulx ||
-     sr.uly!=cfmask.uly)
+     sr.uly!=cfmask.uly) {
     printf("Warning: lndsr and cfmask images do not match!  The number of "
         "lines and samples, along with the UL x and y pixel locations need "
         "to be the same.\n");
+	exit(EXIT_FAILURE);
+  }
 
   /* allocate memory */
   buf = calloc(cfmask.ncols, sizeof(uint8));
   if(!buf) {
     printf("Memory allocation error\n");
-    exit(1);
+	exit(EXIT_FAILURE);
   }
   
   /* create the cfmask band */
   printf("Updating lndsr file for creating/modifying cfmask SDS ...\n");
   if((ret=openForWrite(&sr, &cfmask))==FAILURE) {
     printf("Open for write error\n");
-    exit(1);
+	exit(EXIT_FAILURE);
   }
-
-  if(ret == 0)
-    printf("Appending cfmask for line --- ");
-  else
-    printf("Updating cfmask for line --- ");
+  printf("Appending cfmask for line --- ");
 
   /* loop through each row of data */
   for(irow=0; irow<sr.nrows; irow++) {
@@ -136,12 +149,12 @@ int main(int argc, char *argv[])
 
     if((SDreaddata(cfmask.band_id, start, NULL, length, buf)) == FAILURE) {
       printf("Error: reading cfmask error irow: %d\n", irow);
-      exit(1);
+	  exit(EXIT_FAILURE);
     }
     
     if((SDwritedata(sr.band_id, start, NULL, length, buf)) == FAILURE) {
       printf("Error: writing to combined file error irow: %d\n", irow);
-      exit(1);
+	  exit(EXIT_FAILURE);
     }
   } /* end of irow */
   printf ("\n");
@@ -152,7 +165,7 @@ int main(int argc, char *argv[])
 
   /* Free buffers */
   free(buf);
-  return EXIT_SUCCESS;      /* success */
+  exit(EXIT_SUCCESS);      /* success */
 }
 
 
@@ -166,8 +179,11 @@ void usage(char *command)
 }
 
 
-/* get metadata and open specific sds */
-int getMetaInfo(GRID_SR *sr) {
+/* get metadata and open specific sds
+   processSr specifies if we are reading the surface reflectance product,
+     if not then we are reading the cfmask product
+*/
+int getMetaInfo(GRID_SR *sr, bool processSR) {
 
   int index, ret;
 
@@ -180,28 +196,28 @@ int getMetaInfo(GRID_SR *sr) {
 
   /* open a hdf file */
   gfid = GDopen(sr->fileName, DFACC_READ);
-  if(gfid==FAILURE){
+  if(gfid==HDF_FAILURE){
       printf ("Not successful in retrieving grid file ID/open\n");
       return FAILURE;
   }
 
   /* find out about grid type */
   ngrid=GDinqgrid(sr->fileName, GD_gridlist,&bufsize);
-  if(ngrid==FAILURE){
+  if(ngrid==HDF_FAILURE){
       printf ("Not successful in retrieving grid name list\n");
       return FAILURE;
   }
 
   /* attach grid */
   gid = GDattach(gfid, GD_gridlist);
-  if(gid==FAILURE){
+  if(gid==HDF_FAILURE){
       printf ("Not successful in attaching grid.\n");
       return FAILURE;
   }
 
   /* get grid info */
   ret = GDgridinfo(gid, &sr->ncols, &sr->nrows, GD_upleft, GD_lowright);
-  if(ret==FAILURE){
+  if(ret==HDF_FAILURE){
       printf ("Failed to read grid info.\n");
       return FAILURE;
   }
@@ -210,25 +226,37 @@ int getMetaInfo(GRID_SR *sr) {
 
   /* detach grid */
   ret = GDdetach(gid);
-  if(ret==FAILURE){
+  if(ret==HDF_FAILURE){
       printf ("Failed to detach grid.\n");
       return FAILURE;
   }
 
   /* close for grid access */
   ret = GDclose(gfid);
-  if(ret==FAILURE){
+  if(ret==HDF_FAILURE){
       printf ("GD-file close failed.\n");
       return FAILURE;
   }
 
-  /* open hdf file and get sds_id from given sds_name */  
+  /* open hdf file */ 
   if ((sr->SD_ID = SDstart(sr->fileName, DFACC_READ))<0) {
     printf("Can't open file %s\n",sr->fileName);
     return FAILURE;
   }
 
-  /* open band1 */
+  /* look for the CFMaskVersion in the main attributes of fmask product */
+  if (!processSR) {
+    if ((att_id = SDfindattr(sr->SD_ID, "CFmaskVersion")) == HDF_FAILURE) {
+      printf ("Can't retrieve CFmaskVersion from HDF file.\n");
+      return FAILURE;
+    }
+    else if (SDreadattr(sr->SD_ID, att_id, sr->versionNum) == HDF_FAILURE) {
+      printf ("Can't retrieve CFmaskVersion from HDF file.\n");
+      return FAILURE;
+    }
+  }
+
+  /* open band1 and get sds_id from given sds_name */
   if ((index=SDnametoindex(sr->SD_ID, sr->sdsName))<0) {
     printf("Not successful in convert sdsName %s to index\n", sr->sdsName);
     return FAILURE;
@@ -236,58 +264,64 @@ int getMetaInfo(GRID_SR *sr) {
   sr->band_id = SDselect(sr->SD_ID, index);
 
   /* retrieve SR Fill Value and other attributes */
-  if ((att_id = SDfindattr(sr->band_id, "_FillValue")) == FAILURE) {
+  if ((att_id = SDfindattr(sr->band_id, "_FillValue")) == HDF_FAILURE) {
     printf ("Can't retrieve fill value from SDS %s.\n", sr->sdsName);
     return FAILURE;
   }
-  else if (SDreadattr(sr->band_id, att_id, &sr->fillValue) == FAILURE) {
+  else if (SDreadattr(sr->band_id, att_id, &sr->fillValue) == HDF_FAILURE) {
     printf ("Can't retrieve fill value from SDS %s.\n", sr->sdsName);
     return FAILURE;
   }
 
-  if ((att_id = SDfindattr(sr->band_id, "_SaturateValue")) == FAILURE) {
-    printf ("Can't retrieve saturation value from SDS %s (expected for "
-      "cfmask).\n", sr->sdsName);
-  }
-  else if (SDreadattr(sr->band_id, att_id, &sr->satValue) == FAILURE) {
-    printf ("Can't retrieve saturation value from SDS %s.\n", sr->sdsName);
-    return FAILURE;
+  if (processSR) {
+    if ((att_id = SDfindattr(sr->band_id, "_SaturateValue")) == HDF_FAILURE) {
+      printf ("Can't retrieve saturation value from SDS %s.\n", sr->sdsName);
+      return FAILURE;
+    }
+    else if (SDreadattr(sr->band_id, att_id, &sr->satValue) == HDF_FAILURE) {
+      printf ("Can't retrieve saturation value from SDS %s.\n", sr->sdsName);
+      return FAILURE;
+    }
   }
 
-  if ((att_id = SDfindattr(sr->band_id, "long_name")) == FAILURE) {
+  if ((att_id = SDfindattr(sr->band_id, "long_name")) == HDF_FAILURE) {
     printf ("Can't retrieve long name from SDS %s.\n", sr->sdsName);
     return FAILURE;
   }
-  else if (SDreadattr(sr->band_id, att_id, sr->longName) == FAILURE) {
+  else if (SDreadattr(sr->band_id, att_id, sr->longName) == HDF_FAILURE) {
     printf ("Can't retrieve long name from SDS %s.\n", sr->sdsName);
     return FAILURE;
   }
 
-  if ((att_id = SDfindattr(sr->band_id, "units")) == FAILURE) {
-    printf ("Can't retrieve units from SDS %s (expected for cfmask).\n",
-      sr->sdsName);
-  }
-  else if (SDreadattr(sr->band_id, att_id, sr->units) == FAILURE) {
-    printf ("Can't retrieve units from SDS %s.\n", sr->sdsName);
-    return FAILURE;
+  if (processSR) {
+    if ((att_id = SDfindattr(sr->band_id, "units")) == HDF_FAILURE) {
+      printf ("Can't retrieve units from SDS %s.\n", sr->sdsName);
+      return FAILURE;
+    }
+    else if (SDreadattr(sr->band_id, att_id, sr->units) == HDF_FAILURE) {
+      printf ("Can't retrieve units from SDS %s.\n", sr->sdsName);
+      return FAILURE;
+    }
   }
 
-  if ((att_id = SDfindattr(sr->band_id, "valid_range")) == FAILURE) {
+  if ((att_id = SDfindattr(sr->band_id, "valid_range")) == HDF_FAILURE) {
     printf ("Can't retrieve valid range from SDS %s.\n", sr->sdsName);
     return FAILURE;
   }
-  else if (SDreadattr(sr->band_id, att_id, sr->range) == FAILURE) {
+  else if (SDreadattr(sr->band_id, att_id, sr->range) == HDF_FAILURE) {
     printf ("Can't retrieve valid range from SDS %s.\n", sr->sdsName);
     return FAILURE;
   }
 
-  if ((att_id = SDfindattr(sr->band_id, "scale_factor")) == FAILURE) {
-    printf ("Can't retrieve scale factor from SDS %s (expected for cfmask).\n",
-      sr->sdsName);
-  }
-  else if (SDreadattr(sr->band_id, att_id, &sr->scale) == FAILURE) {
-    printf ("Can't retrieve scale factor from SDS %s.\n", sr->sdsName);
-    return FAILURE;
+  if (processSR) {
+    if ((att_id = SDfindattr(sr->band_id, "scale_factor")) == HDF_FAILURE) {
+      printf ("Can't retrieve scale factor from SDS %s.\n", sr->sdsName);
+      return FAILURE;
+    }
+    else if (SDreadattr(sr->band_id, att_id, &sr->scale) == HDF_FAILURE) {
+      printf ("Can't retrieve scale factor from SDS %s.\n", sr->sdsName);
+      return FAILURE;
+    }
   }
 
 #ifdef DEBUG
@@ -327,7 +361,7 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
         cfmask->sdsName);
   }
 
-  if((SDend(sr->SD_ID)) == FAILURE) {
+  if((SDend(sr->SD_ID)) == HDF_FAILURE) {
     printf("SDend for %s error!\n", sr->fileName);
     return FAILURE;
   }
@@ -335,13 +369,13 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
 
   /* create output use HDF-EOS functions */
   GDfid = GDopen(sr->fileName, DFACC_RDWR);
-  if(GDfid == FAILURE){
+  if(GDfid == HDF_FAILURE){
     printf("Not successful in creating grid file %s", sr->fileName);
     return FAILURE;
   }
   
   GDid = GDattach(GDfid, "Grid");
-  if(GDid == FAILURE) {
+  if(GDid == HDF_FAILURE) {
     printf("Not successful in attaching grid ID/ create for %s", sr->fileName);
     return FAILURE;
   }
@@ -350,7 +384,7 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
   if(exist == 0) {
     ret = GDdeffield(GDid, cfmask->sdsName, "YDim,XDim", DFNT_UINT8,
       HDFE_NOMERGE);
-    if(ret==FAILURE){
+    if(ret==HDF_FAILURE){
       printf ("Not successful in defining %s SDS", cfmask->sdsName);
       return FAILURE;
     }
@@ -358,14 +392,14 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
   
   /* detach grid */
   ret = GDdetach(GDid);
-  if(ret==FAILURE){
+  if(ret==HDF_FAILURE){
     printf ("Failed to detach grid.");
     return FAILURE;
   }
 
   /* close for grid access */
   ret = GDclose(GDfid);
-  if(ret==FAILURE){
+  if(ret==HDF_FAILURE){
     printf ("GD-file close failed.");
     return FAILURE;
   }
@@ -376,6 +410,14 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
     return FAILURE;
   }
   
+  /* write the fmask version */
+  ret = SDsetattr(sr->SD_ID, "CFmaskVersion", DFNT_CHAR8,
+    strlen(cfmask->versionNum), cfmask->versionNum);
+  if (ret == HDF_FAILURE) {
+    printf ("Can't write CFmaskVersion to global attributes.\n");
+    return FAILURE;
+  }
+
   /* get sds_id from given sds_name and then write metedata */
   if ((index = SDnametoindex(sr->SD_ID, cfmask->sdsName))<0) {
     printf("Unable to find the newly created SDS (%s) in the surface "
@@ -386,21 +428,21 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
 
   /* write SDS metadata */
   ret = SDsetattr(sr->band_id, "long_name", DFNT_CHAR8,
-      strlen(cfmask->longName), cfmask->longName);
-  if (ret == FAILURE) {
+    strlen(cfmask->longName), cfmask->longName);
+  if (ret == HDF_FAILURE) {
     printf("Can't write SR long_name for SDS %s\n", cfmask->sdsName);
     return FAILURE;
   } 
     
   ret = SDsetattr(sr->band_id, "valid_range", DFNT_INT16, 2, cfmask->range);
-  if (ret == FAILURE) {
+  if (ret == HDF_FAILURE) {
     printf ("Can't write SR valid_range for SDS %s", cfmask->sdsName);
     return FAILURE;
   } 
   
   ret = SDsetattr(sr->band_id, "_FillValue", DFNT_INT16, 1,
     &(cfmask->fillValue));
-  if (ret == FAILURE) {
+  if (ret == HDF_FAILURE) {
     printf ("Can't write SR _FillValue for SDS %s", cfmask->sdsName);
     return FAILURE;
   } 
@@ -408,7 +450,7 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
   sprintf (lndsr_index, "0 clear; 1 water; 2 cloud_shadow; 3 snow; 4 cloud");
   ret = SDsetattr(sr->band_id, "mask_index", DFNT_CHAR8, strlen(lndsr_index),
     lndsr_index);
-  if (ret == FAILURE) {
+  if (ret == HDF_FAILURE) {
     printf ("Can't write SR mask_index for SDS %s", sr->sdsName);
     return FAILURE;
   } 
@@ -426,19 +468,19 @@ int openForWrite(GRID_SR *sr, GRID_SR *cfmask)
   }
   cfmask->band_id = SDselect(cfmask->SD_ID, index);
 
-  return exist;
+  return SUCCESS;
 }
 
 
 int closeUp(GRID_SR *sr, bool close) 
 {
-  if((SDendaccess(sr->band_id)) == FAILURE) {
+  if((SDendaccess(sr->band_id)) == HDF_FAILURE) {
     printf("SDendaccess for %s cfmask band error!\n", sr->fileName);
     return FAILURE;
   }
 
   if (close) {
-    if((SDend(sr->SD_ID)) == FAILURE) {
+    if((SDend(sr->SD_ID)) == HDF_FAILURE) {
       printf("SDend for %s error!\n", sr->fileName);
       return FAILURE;
     }
